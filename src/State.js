0d98c5801902731d2c7bca7260a8e2b9413767d9
Gaelan D'costa <gdcosta@gmail.com>
2017-06-10 17:26:33 -0400

    Basic example state to work with

---
+++ src/State.js
@@ -0,0 +1,34 @@
+/* @flow */
+
+export class Employee {
+    name: string;
+    balance: number;
+
+    constructor(name: string) {
+	this.name = name;
+	this.balance = 0.0;
+    }
+}
+
+export class Snack {
+    name: string;
+    price: number;
+
+    constructor(name: string, price: number) {
+	this.name = name;
+	this.price = price;
+    }
+}
+
+export let employees = [
+    new Employee('Chris Spahr'),
+    new Employee('Phil Cairns'),
+    new Employee('Phil Nicolcev'),
+    new Employee("Gaelan D'costa")
+];
+
+export let snacks = [
+    new Snack('Perrier', 2.00),
+    new Snack('Dr. Pepper', 0.50),
+    new Snack('Soylent', 2.00)
+];
